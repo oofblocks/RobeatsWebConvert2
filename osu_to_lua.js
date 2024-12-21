1665e6cc9d9cf88a9ac9489e8bcdb9727ccd57f2
oofblox <80658332+oofblocks@users.noreply.github.com>
2024-12-21 00:45:25 -0500

    Update osu_to_lua.js

--- osu_to_lua.js
+++ osu_to_lua.js
@@ -96,43 +96,44 @@ module.export("osu_to_lua", function(osu_file_contents) {
 		return !(_i_to_removes[i])
 	})
 
-	append_to_output("local rtv = {}");
+	append_to_output("local Map = {}");
+	append_to_output("Map.Notes = {{}, {}, {}, {}}");
+	append_to_output(format("Map.%s = %d","Offset",0));
 	//append_to_output("--Your audio assetid should be in the form of \"rbxassetid://...\". Upload audios at \"https://www.roblox.com/develop?View=3\", and copy the uploaded id from the URL.")
-	append_to_output(format("rtv.%s = \"%s\"","AudioAssetId","rbxassetid://FILL_IN_AUDIO_ASSETID_HERE"));
+	append_to_output(format("Map.%s = \"%s\"","AudioAssetId","rbxassetid://FILL_IN_AUDIO_ASSETID_HERE"));
 	//append_to_output("--The name of your map.")
-	append_to_output(format("rtv.%s = \"%s\"","AudioFilename",beatmap.Title));
+	append_to_output(format("Map.%s = \"%s\"","Name",beatmap.Title));
 	//append_to_output("--The artist of your map.")
-	append_to_output(format("rtv.%s = \"%s\"","AudioArtist",""));
+	append_to_output(format("Map.%s = \"%s\"","Artist",""));
 	//append_to_output("--A description of your map. Will be displayed in the song select list by default.")
-	append_to_output(format("rtv.%s = \"%s\"","AudioDescription",""));
+	//append_to_output(format("rtv.%s = \"%s\"","AudioDescription",""));
 	//append_to_output("--Your cover art assetid should be in the form of \"rbxassetid://...\". Upload images at \https://www.roblox.com/develop?View=13\".")
 	//append_to_output("--The uploaded id is the DECAL id, you must use the TEXTURE id here. To get the TEXTURE id, paste the full URL of the DECAL id into the Image property of an ImageLabel. There is a debug ImageLabel under ServerStorage in the mapping test place.")
-	append_to_output(format("rtv.%s = \"%s\"","AudioCoverImageAssetId","rbxassetid://FILL_IN_COVERART_TEXTURE_ASSETID_HERE"));
+	append_to_output(format("Map.%s = \"%s\"","Background","rbxassetid://FILL_IN_COVERART_TEXTURE_ASSETID_HERE"));
 	
 	//append_to_output("--The difficulty number of your map.")
-	append_to_output(format("rtv.%s = %d","AudioDifficulty",1));
+	//append_to_output(format("rtv.%s = %d","AudioDifficulty",1));
 	//append_to_output("--The audio time offset of your map in milliseconds. The default value is -75ms.")
-	append_to_output(format("rtv.%s = %d","AudioTimeOffset",-75));
 	//append_to_output("--The audio volume of your map. The default value is 0.5.")
-	append_to_output(format("rtv.%s = %d","AudioVolume",0.5));
+	//append_to_output(format("rtv.%s = %d","AudioVolume",0.5));
 	//append_to_output("--The speed at which the notes arrive (in milliseconds) for your map. The default value is 1500ms, or 1.5 seconds from spawn to hit.")
-	append_to_output(format("rtv.%s = %d","AudioNotePrebufferTime",1500));
+	//append_to_output(format("rtv.%s = %d","AudioNotePrebufferTime",1500));
 	//append_to_output("--The map modifier. By default in the mapping place, 0 is normal mode and 1 is a hard mode (which is marked as \"Supporters Only\").")
-	append_to_output(format("rtv.%s = %d","AudioMod",0));
+	//append_to_output(format("rtv.%s = %d","AudioMod",0));
 	//append_to_output("--The hit SFX group of your map. Valid values are 0 to 5. Add your own hit SFX in ReplicatedStorage.RobeatsGameCore.HitSFXGroup")
-	append_to_output(format("rtv.%s = %d","AudioHitSFXGroup",0));
+	//append_to_output(format("rtv.%s = %d","AudioHitSFXGroup",0));
 	
-	append_to_output(`rtv.BPM = ${beatmap.bpmMin == beatmap.bpmMax  && beatmap.bpmMin | `${beatmap.bpmMin} - ${beatmap.bpmMax}`}`)
-	append_to_output(`rtv.Tags = "${beatmap.Tags}"`)
-	append_to_output(`rtv.Mapper = "${beatmap.Creator}"`)
-	append_to_output(`rtv.Difficulty = "${beatmap.Version}"`)
-	append_to_output(`rtv.Notes = ${beatmap.nbCircles}`)
-	append_to_output(`rtv.Sliders = ${beatmap.nbSliders}`)
+	append_to_output(`Map.BPM = ${beatmap.bpmMin == beatmap.bpmMax  && beatmap.bpmMin | `${beatmap.bpmMin} - ${beatmap.bpmMax}`}`)
+	append_to_output(`Map.Tags = "${beatmap.Tags}"`)
+	append_to_output(`Map.Mapper = "${beatmap.Creator}"`)
+	//append_to_output(`rtv.Difficulty = "${beatmap.Version}"`)
+	//append_to_output(`rtv.Notes = ${beatmap.nbCircles}`)
+	//append_to_output(`rtv.Sliders = ${beatmap.nbSliders}`)
 	append_to_output(`rtv.Key = script.Name`)
 	
 	append_to_output("rtv.HitObjects = {}")
-	append_to_output("local function note(time,track) rtv.HitObjects[#rtv.HitObjects+1]={Time=time;Type=1;Track=track;} end")
-	append_to_output("local function hold(time,track,duration) rtv.HitObjects[#rtv.HitObjects+1] = {Time=time;Type=2;Track=track;Duration=duration;}  end")
+	append_to_output("local function Note(lane, ms) table.insert(Map.Notes[lane], ms) end")
+	append_to_output("local function Hold(lane, start, duration) table.insert(Map.Notes[lane], {start, finish}) end")
 	append_to_output("--")
 	
 	for (var i = 0; i < beatmap.hitObjects.length; i++) {
@@ -141,20 +142,20 @@ module.export("osu_to_lua", function(osu_file_contents) {
 		var track = hitobj_x_to_track_number(itr.position[0]);
 
 		if (type == "slider") {
-			append_to_output(format("hold(%d,%d,%d) ", itr.startTime, track, itr.duration))
+			append_to_output(format("Hold(%d,%d,%d) ", track, itr.startTime, itr.duration))
 		} else {
-			append_to_output(format("note(%d,%d) ",itr.startTime, track))
+			append_to_output(format("Note(%d,%d) ", track, itr.startTime))
 		}
 	}
 	append_to_output("--")
 
-	append_to_output("rtv.TimingPoints = {")
-	for (var i = 0; i < beatmap.timingPoints.length; i++) {
-		var itr = beatmap.timingPoints[i];
-		append_to_output(format("\t[%d] = { Time = %d; BeatLength = %d; };",i+1, itr.offset, itr.beatLength))
-	}
-	append_to_output("};")
-	append_to_output("return rtv")
+	//append_to_output("rtv.TimingPoints = {")
+	//for (var i = 0; i < beatmap.timingPoints.length; i++) {
+	//	var itr = beatmap.timingPoints[i];
+	//	append_to_output(format("\t[%d] = { Time = %d; BeatLength = %d; };",i+1, itr.offset, itr.beatLength))
+	//}
+	//append_to_output("};")
+	append_to_output("return Map")
 
 	return rtv_lua
 })
